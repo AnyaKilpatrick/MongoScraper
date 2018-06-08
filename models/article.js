8c91f48d4356ea8043bfe371125fa13cdbe720a2
Anya <hannakilpatrick@gmail.com>
2018-06-08 12:57:24 -0400

    Article model

---
+++ models/article.js
@@ -0,0 +1,38 @@
+var mongoose = require("mongoose");
+
+// saving a reference to the schema constructor
+var Schema = mongoose.Schema;
+
+// creating a new ArticleSchema object
+ var ArticleSchema = new Schema({
+    // title
+    title:{
+        type:String,
+        required:true
+    },
+    // link to article
+    articleLink:{
+        type: String,
+        required:true
+    },
+    // summary
+    summary:{
+        type: String,
+        required:true
+    },
+    // link to img
+    imgLink:{
+        type: String,
+        required:false
+    },
+    // adding an id of the note/review associated with this article
+    note:{
+        type:Schema.Types.ObjectId,
+        ref:"Note"
+    }
+ });
+
+var Article = mongoose.model("Article", ArticleSchema);
+
+// exporting Article model
+module.exports = Article;
