5f3e016ce5f746f1f46d75557ec0b05945edc5f7
Anya <hannakilpatrick@gmail.com>
2018-06-09 14:38:02 -0400

    change PORT

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ var axios = require("axios");
 var cheerio = require("cheerio");
 
 
-var PORT = process.env.PORT||3000;
+var PORT = process.env.PORT || 3000;
 
 var app = express();
 // requiring models
